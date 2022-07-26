6f9d0fedb6f5296118f421ad64c015f62d8da69d
Issam <issam.hedhli@ensi-uma.tn>
2022-07-26 21:03:57 +0100

    yeeeeey

--- app/src/main/java/com/example/talancantine/FragmentAdmin/ThirdFragment.kt
+++ app/src/main/java/com/example/talancantine/FragmentAdmin/ThirdFragment.kt
@@ -1,14 +1,19 @@
 package com.example.talancantine.FragmentAdmin
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.talancantine.R
+import kotlinx.android.synthetic.main.fragment_third.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ThirdFragment : Fragment() {
-
+    var formatDate= SimpleDateFormat("dd MMMM YYYY", Locale.US)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +23,26 @@ class ThirdFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_third, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // date de la creation de menu du jour
+        button_date_picked.setOnClickListener(View.OnClickListener {
+            val getDate : Calendar = Calendar.getInstance()
+            val datepicker= DatePickerDialog(requireContext(), android.R.style.Theme_Holo_Light_Dialog_MinWidth, DatePickerDialog.OnDateSetListener{ datepicker, i, i2, i3->
 
-}
+
+                val  selectDate: Calendar = Calendar.getInstance()
+                selectDate.set(Calendar.YEAR,i)
+                selectDate.set(Calendar.MONTH,i2)
+                selectDate.set(Calendar.DAY_OF_MONTH,i3)
+                val date : String = formatDate.format(selectDate.time)
+                Toast.makeText(context," Date : "+date, Toast.LENGTH_SHORT).show()
+                txt_view_date_picked.text=date
+
+            }, getDate.get(Calendar.YEAR), getDate.get(Calendar.MONTH), getDate.get(Calendar.DAY_OF_MONTH))
+            datepicker.show()
+        })
+
+    }
+    }
+
