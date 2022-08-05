36046d0a2a3709687690d8f98e0c96e99e2d63e7
Issam <issam.hedhli@ensi-uma.tn>
2022-08-05 18:49:11 +0100

    :/

--- app/src/main/java/com/example/talancantine/FirstFragmentUser.kt
+++ app/src/main/java/com/example/talancantine/FirstFragmentUser.kt
@@ -1,22 +1,20 @@
 package com.example.talancantine
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.SearchView
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.talancantine.databinding.FragmentFirstUserBinding
-import kotlinx.android.synthetic.main.activity_sign_in.*
-import kotlinx.android.synthetic.main.fragment_first_user.*
 
 
 class FirstFragmentUser : Fragment() {
     private var _binding: FragmentFirstUserBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,48 +25,7 @@ class FirstFragmentUser : Fragment() {
           _binding= FragmentFirstUserBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val user = arrayOf("Entrée","Plat principal","Dessert")
-
-        val adapter = activity?.let {
-            ArrayAdapter<String>(
-                it,
-                android.R.layout.simple_list_item_1,
-                user
-
-            )
-
-
-
-    }
-
-        binding.menuList.adapter = adapter
-
-
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                binding.searchView.clearFocus()
-                if (user.contains(query)){
-
-                    adapter?.filter?.filter(query)
-
-                }
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                adapter?.filter?.filter(newText)
-                return false
-            }
-
-
-        })
-
-    }
-
-
-
-
 }
+
+
+
