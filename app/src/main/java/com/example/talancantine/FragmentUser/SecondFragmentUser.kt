ac52c4341ace6935d8aec46e59d7e5d89177864e
Issam <issam.hedhli@ensi-uma.tn>
2022-08-18 12:37:43 +0100

    OKEY

--- app/src/main/java/com/example/talancantine/FragmentUser/SecondFragmentUser.kt
+++ app/src/main/java/com/example/talancantine/FragmentUser/SecondFragmentUser.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.talancantine.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.talancantine.adapter.CartAdapter
+import com.example.talancantine.adapter.RecyclerAdapterCategory
+import com.example.talancantine.databinding.FragmentSecondUserBinding
+import kotlinx.android.synthetic.main.fragment_second_user.*
 
 
 class SecondFragmentUser : Fragment() {
+    private lateinit var binding: FragmentSecondUserBinding
 
+    private var adapterCart: CartAdapter?= null
+    private var linearLayoutManagerCart: RecyclerView.LayoutManager?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,11 +25,18 @@ class SecondFragmentUser : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_second_user, container, false)
+        binding= FragmentSecondUserBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        linearLayoutManagerCart= LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        rv_cart.layoutManager=linearLayoutManagerCart
+
+        adapterCart= CartAdapter()
+        rv_cart.adapter=adapterCart
 
     }
 
---
+++ app/src/main/java/com/example/talancantine/adapter/CartAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.talancantine.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.talancantine.R
+
+class CartAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var titles= arrayListOf("PIZZA","BURGER","SANDWICH","DRINKS ")
+    private var images= intArrayOf(R.drawable.pizza, R.drawable.burger, R.drawable.sandwich, R.drawable.drink)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_rv_cart,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        holder.itemView.findViewById<TextView>(R.id.tv_dish_name).text=titles[position]
+        holder.itemView.findViewById<ImageView>(R.id.img_dish).setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+
+
+        init {
+            itemImage=itemView.findViewById(R.id.img_dish)
+            itemTitle=itemView.findViewById(R.id.tv_dish_name)
+
+
+        }
+    }
+
+}
